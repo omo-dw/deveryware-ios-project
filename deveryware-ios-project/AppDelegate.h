31a8815937b9e9dfaac802e98a578d51ee128ee0
Aymen Abbassia <aymen.abbassia@soat.fr>
2016-01-07 11:40:08 +0100

    First Commit

---
+++ deveryware-ios-project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  deveryware-ios-project
+//
+//  Created by Aymen Abbassia on 07/01/2016.
+//  Copyright © 2016 SOAT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
