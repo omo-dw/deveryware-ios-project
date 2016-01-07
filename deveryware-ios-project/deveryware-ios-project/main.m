40cfd6b4af82a3a5b6cdfa56e41a4f4d5deaa793
Aymen Abbassia <aymen.abbassia@soat.fr>
2016-01-07 11:16:32 +0100

    First Commit

---
+++ deveryware-ios-project/deveryware-ios-project/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  deveryware-ios-project
+//
+//  Created by Aymen Abbassia on 07/01/2016.
+//  Copyright © 2016 SOAT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
